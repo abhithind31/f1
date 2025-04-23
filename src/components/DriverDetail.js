6af8812ac425362a5596c6e99353d4bcb941bc5e
abhithind31 <abhithind31@gmail.com>
2025-04-23 13:48:28 +0530

    Feature Added

--- src/components/DriverDetail.js
+++ src/components/DriverDetail.js
@@ -10,8 +10,10 @@ function DriverDetail({ driver, onClose }) {
       <h2>{driver.name} Details</h2>
       <p><strong>Team:</strong> {driver.team}</p>
       <p><strong>Country:</strong> {driver.country}</p>
-      {/* Points will be added later */}
-      {/* <p><strong>Points:</strong> {driver.points}</p> */}
+      <p><strong>Points:</strong> {driver.points}</p>
+      {/* Display new stats */}
+      <p><strong>Wins:</strong> {driver.wins}</p>
+      <p><strong>Podiums:</strong> {driver.podiums}</p>
       <button onClick={onClose}>Close</button>
     </div>
   );
